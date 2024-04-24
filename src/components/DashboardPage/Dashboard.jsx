0810a3092ff03d8dcf7ee940c55324fd745f5255
J1MMM <jimuelbarero00@gmail.com>
2024-04-24 21:18:39 +0800

    april 24 2024

--- src/components/DashboardPage/Dashboard.jsx
+++ src/components/DashboardPage/Dashboard.jsx
@@ -144,7 +144,7 @@ const Dashboard = () => {
             width: "100%",
             display: "flex",
             justifyContent: "center",
-            bgcolor: "red",
+            bgcolor: "#FFF",
             borderRadius: 3,
             flexDirection: "column",
             boxSizing: "border-box",
