bb118ce99935f9709ada24b2ce1240024b6d9ffd
Jimuel Baraero <102773073+J1MMM@users.noreply.github.com>
2024-08-12 11:56:44 +0800

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:3500";
 // const BASE_URL = "https://spctmo-server.onrender.com";
 // const BASE_URL = "https://kapitolyo-api-server.onrender.com";
-const BASE_URL = "https://kapitolyo-api-server-1.onrender.com/";
+const BASE_URL = "https://kapitolyo-api-server-1.onrender.com";
 
 
 export default axios.create({
