dda62dfd1a668890ff47145578ff09ff2f742629
J1MMM <jimuelbaraero00@gmail.com>
2024-08-19 11:06:41 +0800

    cashier receipt add mtop at the bttom

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500";
+// const BASE_URL = "http://localhost:3500";
 // const BASE_URL = "https://spctmo-server.onrender.com";
 // const BASE_URL = "https://kapitolyo-api-server.onrender.com";
 // const BASE_URL = "https://kapitolyo-api-server-1.onrender.com";
 // const BASE_URL = "https://kapitolyo-api-server-2.onrender.com";
-// const BASE_URL = "https://spctmo-server-gitibbeqra-uc.a.run.app";
+const BASE_URL = "https://spctmo-server-gitibbeqra-uc.a.run.app";
 
 export default axios.create({
   baseURL: BASE_URL,
